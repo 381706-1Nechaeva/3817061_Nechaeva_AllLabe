fb4fe27f8e39375b6df6e123299baa3d1650e411
Ekaterina Nechaeva <katene10@mail.ru>
2018-11-27 20:07:20 +0300

    Добавила конст

--- vector/c2.cpp
+++ vector/c2.cpp
@@ -1,19 +1,35 @@
-
-#include <iostream>
+ï»¿#include <iostream>
 
 #include "vector.h"
 
 int main()
 {
-	TVector<int> C;
-	TVector<int> Sum;
-	TVector<float> Razn;
-	TVector<float> Umn;
-	TVector<float> Del;
-	std::cout <<"Ââåäèòå ðàçìåð âåêòîðà\n";
-	std::cin >> C;
-	std::cout << C;// << "\n" << B << "\n" << C << "\n" << D << "\n";
+	//TVector<int> D(6,2);
+	//TVector<int> C,F;
+	//TVector<int> Sum;
+	//TVector<float> Razn;
+	//TVector<float> Umn;
+	//TVector<float> Del;
+	//std::cout <<"Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã°Ã Ã§Ã¬Ã¥Ã° Ã¢Ã¥ÃªÃ²Ã®Ã°Ã \n";
+	//std::cin >> C >> D;
+	//F=C;
+	//if(F!=C)
+	//	cout << "neRavny\n";
+	//else 
+	//	cout << " ravny\n";
+	//std::cout << C <<"\n" << D;// << "\n" << B << "\n" << C << "\n" << D << "\n";
 
 
+	//return 0;
+	TMatrix<int> D(2);
+	TMatrix<int> F(D), C(D);
+	cout << "Vvedite matrizy s razmerom = " << F.GetSize() << "\n";
+	cin >> F;
+	D = F;
+	C = D + F;
+	cout << C;
+	/*if (D != F)
+		cout << "net";
+	else cout << "ravn";*/
 	return 0;
 }
