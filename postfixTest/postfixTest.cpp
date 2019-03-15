055b0313fb4291b1142a445ece304aa4f5df5f4b
3817061nechaeva <katene10@mail.ru>
2019-03-15 19:53:00 +0300

    All1

--- postfixTest/postfixTest.cpp
+++ postfixTest/postfixTest.cpp
@@ -4,30 +4,26 @@
 
 TEST(TPostfix, throw_when_different_number_of_brackets)
 {
-  char* str = new char[255];
-  str = "((1+1)";
+  char str[] = "((1+1)";
   TPostfix pol(str);
   ASSERT_ANY_THROW(pol.Postfix());
 }
 TEST(TPostfix, throw_when_the_wrong_symbol)
 {
-  char* str = new char[255];
-  str = "1+3+&";
+  char str[] = "1+3+&";
   TPostfix pol(str);
   ASSERT_ANY_THROW(pol.Postfix());
 }
 TEST(TPostfix, throw_when_operations_more_then_operands)
 {
-  char* str = new char[255];
-  str = "1++3";
+  char str[] = "1++3";
   TPostfix pol(str);
   pol.Postfix();
   ASSERT_ANY_THROW(pol.calculate());
 }
 TEST(TPostfix, can_add_two_numbers)
 {
-  char* str = new char[255];
-  str = "1+3";
+  char str[] = "1+3";
   TPostfix pol(str);
   pol.Postfix();
   EXPECT_EQ(4, pol.calculate());
@@ -42,32 +38,28 @@ TEST(TPostfix, can_multiply_two_numbers)
 }
 TEST(TPostfix, can_subtract_two_numbers)
 {
-  char* str = new char[255];
-  str = "100-50";
+  char str[] = "100-50";
   TPostfix pol(str);
   pol.Postfix();
   EXPECT_EQ(50, pol.calculate());
 }
 TEST(TPostfix, can_divide_two_numbers)
 {
-  char* str = new char[255];
-  str = "10/5";
+  char str[] = "10/5";
   TPostfix pol(str);
   pol.Postfix();
   EXPECT_EQ(2, pol.calculate());
 }
 TEST(TPostfix, can_calculate_all_operations)
 {
-  char* str = new char[255];
-  str = "(1+3)-100*2/50";
+  char str[] = "(1+3)-100*2/50";
   TPostfix pol(str);
   pol.Postfix();
   EXPECT_EQ(0, pol.calculate());
 }
 TEST(TPostfix, can_calculate_with_negative_numbers)
 {
-  char* str = new char[255];
-  str = "100*2-(-100+30)";
+  char str[] = "100*2-(-100+30)";
   TPostfix pol(str);
   pol.Postfix();
   EXPECT_EQ(270, pol.calculate());
