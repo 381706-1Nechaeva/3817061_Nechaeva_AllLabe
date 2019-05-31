7a605d59bba0baba2002e5dcd8dc9463e09eaaea
3817061nechaeva <katene10@mail.ru>
2019-05-31 21:52:20 +0300

    TTT

---
+++ libtext/text.h
@@ -0,0 +1,22 @@
+﻿#include "nodeiter.h"
+
+class TText
+{
+protected:
+  TNode *root;
+public:
+  TText();
+  TText(TNode* r);
+  TText(TText& T);
+  TNode* GetRoot();
+  char* Copy(int start, int n);
+  void Del(TNode* start, int n);
+  void Ins(TNode* start, TNode* d); // âñòàâêà
+  void Ins(TNode* start, string S);
+  void Ins(TNode* start, char c);
+  TNode* Find(char* a); // ïîèñê ñ âîçðàòîì óçëà
+  int FindIndex(char* a); // ïîèñê ñ âîçâðàòîì íîìåðà áóêâû
+  int GetCount();
+  friend ostream& operator<<(ostream& ostr, const TText &T);
+
+};
